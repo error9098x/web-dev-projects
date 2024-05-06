596f98278cff2b5e964db0328df92894a87f8126
error9098x <21f2000172@ds.study.iitm.ac.in>
2024-05-06 09:29:18 +0000

    hello

--- tutorial-03-component/src/components/Form.js
+++ tutorial-03-component/src/components/Form.js
@@ -10,19 +10,24 @@ function MyForm() {
   }
 
   return(
-    <form onSubmit={handleSubmit}>
-      <label>
-        Name:
-        <input type="text" value={name} onChange={e => setName(e.target.value)}  required/>
-      </label>
-      <br/>
-      <label>
-        Email:
-        <input type="email" value={email} onChange={e => setEmail(e.target.value)} required/>
-      </label>
-      <br/>
-      <input type="submit" value="Submit" />
-    </form>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Name:
+          <input type="text" value={name} onChange={e => setName(e.target.value)}  required/>
+        </label>
+        <br/>
+        <label>
+          Email:
+          <input type="email" value={email} onChange={e => setEmail(e.target.value)} required/>
+        </label>
+        <br/>
+        <input type="submit" value="Submit" />
+      </form>
+      <h2>Displaying useState Data:</h2>
+      <p>Name: {name}</p>
+      <p>Email: {email}</p>
+    </div>
   )
 }
 
